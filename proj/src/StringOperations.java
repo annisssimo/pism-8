158ec613c2a6e85c2d02a0f81ec334be4732b430
annisssimo <anyalar04@gmail.com>
2023-11-30 19:49:05 +0300

    feat: add class StringOperations

---
+++ proj/src/StringOperations.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class StringOperations {
+    private ArrayList<String> stringList;
+
+    public StringOperations() {
+        this.stringList = new ArrayList<>();
+    }
+
+}
