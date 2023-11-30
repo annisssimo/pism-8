76a08228ef64f1bc20bb736e8e21a8e42146a8f1
annisssimo <anyalar04@gmail.com>
2023-11-30 21:00:56 +0300

    feat: add functions addObject, removeObject and displayCollection

--- proj/src/StringOperations.java
+++ proj/src/StringOperations.java
@@ -8,4 +8,31 @@ public class StringOperations {
         this.stringList = new ArrayList<>();
     }
 
+    // 1. Добавление и удаление объектов
+    public void addObject(String str) {
+        stringList.add(str);
+    }
+
+    public void removeObject(String str) {
+        stringList.remove(str);
+    }
+
+    public void displayCollection() {
+        for (String str : stringList) {
+            System.out.println(str);
+        }
+    }
+
+    public static void main(String[] args) {
+        StringOperations stringOps = new StringOperations();
+
+        stringOps.addObject("eefe");
+        stringOps.addObject("Woefrld");
+
+        stringOps.removeObject("eefe");
+
+        System.out.println("Original Collection:");
+        stringOps.displayCollection();
+    }
+
 }
